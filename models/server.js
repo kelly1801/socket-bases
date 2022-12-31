14a4c395f06a422f2a90690220d1817fae16b838
Kelly <fabianachara@gmail.com>
2022-12-31 11:53:42 -0500

    conect socket.io

--- models/server.js
+++ models/server.js
@@ -1,33 +1,30 @@
 import express from "express";
 import cors from "cors";
-
+import { createServer } from "http";
+import { Server as socketServer } from "socket.io";
 export class Server {
   constructor() {
-    this.app = express();
-    this.port = process.env.PORT;
-    this.paths = {};
+    this.app    = express();
+    this.port   = process.env.PORT || 3000;
+    this.paths  = {};
+    this.server = createServer(this.app);
+    this.io     = new socketServer(this.server);
 
-  
     this.middlewares();
     this.routes();
   }
-
-
-
-
 
   middlewares() {
     // cors
     this.app.use(cors());
     // public folder
     this.app.use(express.static("public"));
- 
   }
 
   routes() {}
 
   listenPort() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log(`server running on port: ${this.port}`);
     });
   }
